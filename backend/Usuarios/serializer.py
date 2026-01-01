4838a50724e5dc1224a533d176a5c534a442abcb
joshua <condorenajoshua98@gmail.com>
2025-12-31 20:00:04 -0500

    Se implemento el serializaer del ususario

---
+++ backend/Usuarios/serializer.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+User=get_user_model()
+
+class RegisterUserSerealizaer(serializers.ModelSerializer):
+    class Meta:
+        model=User
+        fields=['email','password','username','first_name','last_name']
+    def create(self,validate_data):
+        return User.objects.create(**validate_data)
