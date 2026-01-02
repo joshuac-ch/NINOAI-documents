7e5e29de0242dc60301d9b890f644d8578770371
joshua <condorenajoshua98@gmail.com>
2026-01-01 19:49:32 -0500

    Se modifico el usuarios el api view

--- backend/Usuarios/views.py
+++ backend/Usuarios/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from Usuarios.models import User
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializer import RegisterUserSerealizaer
 # Create your views here.
+class RegisterView(APIView):
+    permission_classes=[]
+
+    def post(self,request):
+        sereliazer=RegisterUserSerealizaer(data=request.data)
+        if(sereliazer.is_valid()):
+            sereliazer.save()
+            return Response({"message":"Usuario creado"},status=201)
+        return Response(sereliazer.errors,status=400)
+
+    
