6ae4dbff0da7941e336645993700fa2077a0e705
joshua <condorenajoshua98@gmail.com>
2026-01-05 19:47:19 -0500

    Se modifico el modelo documentos

--- backend/Documentos/models.py
+++ backend/Documentos/models.py
@@ -1,5 +1,8 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
 class DocumentosModel(models.Model):
-    name=models.CharField()
+    user=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,null=True,blank=True)
+    file=models.FileField(upload_to="documents/",blank=True,null=True)
+    text=models.TextField(blank=True)
+    created_at=models.DateTimeField(auto_now_add=True)
