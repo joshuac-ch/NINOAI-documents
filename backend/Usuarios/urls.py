c8af38f12df3c0da247fb412c2c48cd082690eb9
joshua <condorenajoshua98@gmail.com>
2026-01-07 15:59:53 -0500

    Se modifico las url de usuarios

--- backend/Usuarios/urls.py
+++ backend/Usuarios/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path,include
-from .views import RegisterView,Meview
+from .views import RegisterView,Meview,ShowUser
 urlpatterns = [
     path("register/",RegisterView.as_view()),
-    path("api/auth/me/",Meview.as_view())    
+    path("api/auth/me/",Meview.as_view()),  
+    path("show/user/<int:id>/",ShowUser.as_view())  
 ]
