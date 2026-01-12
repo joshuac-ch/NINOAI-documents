2bcdc45722c846bc4d64fde20b7e07b3e9930fe7
joshua <condorenajoshua98@gmail.com>
2026-01-11 20:33:56 -0500

    Se implemenot el perfile del ussuario

---
+++ fronted/src/Profile/ProfilePage.tsx
@@ -0,0 +1,13 @@
+
+
+export default function ProfilePage() {
+  return (
+    <>
+    <div className="">
+        <div className="">
+            <h1>Perfil del usuario</h1>
+        </div>
+    </div>
+    </>
+  )
+}
