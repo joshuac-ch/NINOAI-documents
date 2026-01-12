e3ae87a39520685c071d7a0567b98465ae5e3f77
joshua <condorenajoshua98@gmail.com>
2026-01-11 20:34:29 -0500

    Se implemento la seccion favoritos

---
+++ fronted/src/Favorite/FavoritePage.tsx
@@ -0,0 +1,13 @@
+
+
+export default function FavoritePage() {
+  return (
+    <>
+    <div className="">
+        <div className="">
+            <h1>Favoritos</h1>
+        </div>
+    </div>
+    </>
+  )
+}
