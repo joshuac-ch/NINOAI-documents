921c46ebdd34c29816f3983ac00893eb52da95c2
joshua <condorenajoshua98@gmail.com>
2026-01-13 18:48:39 -0500

    Se modificaron las rutas

---
+++ fronted/src/App.tsx
@@ -0,0 +1,30 @@
+
+import './App.css'
+import { Route, Routes } from 'react-router-dom'
+
+import LoginPage from './Login/LoginPage'
+import IndexPage from './Home/IndexPage'
+import LeftPage from './layout/LeftPage'
+import RegisterLogin from './Login/RegisterLogin'
+import SettingPage from './Settings/SettingPage'
+import FavoritePage from './Favorite/FavoritePage'
+import ProfilePage from './Profile/ProfilePage'
+
+function App() {  
+
+  return (
+    <Routes>
+      <Route path='/login' element={<LoginPage></LoginPage>}></Route>
+      <Route path='/register' element={<RegisterLogin></RegisterLogin>}></Route>
+      <Route path='/' element={<LeftPage></LeftPage>}>
+         <Route path='/home' element={<IndexPage></IndexPage>}></Route>
+         <Route path='/settings' element={<SettingPage></SettingPage>}></Route>
+         <Route path='/favorite' element={<FavoritePage></FavoritePage>}></Route>
+         <Route path='/profile' element={<ProfilePage></ProfilePage>}></Route>
+     </Route>
+    </Routes>
+   
+  )
+}
+
+export default App
