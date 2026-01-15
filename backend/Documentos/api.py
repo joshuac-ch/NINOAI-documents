a38ee858230a59c3206699fbea0489a56c50ac37
joshua <condorenajoshua98@gmail.com>
2026-01-14 19:49:22 -0500

    Se elimino los scripts innesesarios

--- backend/Documentos/api.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import serializers
-from Documentos.models import DocumentosModel
-class DocumentSerealizer(serializers.ModelSerializer):
-    class Meta:
-        model=DocumentosModel,
-        fields=['nombre']
