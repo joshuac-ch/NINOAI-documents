7f012ec07f4b524f9934f90efeef20faab36e175
joshua <condorenajoshua98@gmail.com>
2026-01-17 20:39:10 -0500

    Se implemento definio la forma de demostrar los datos con un serrializar

---
+++ backend/Documentos/serealizer.py
@@ -0,0 +1,23 @@
+
+from .models import DocumentosModel 
+from rest_framework import serializers
+class DocumentSerealizer(serializers.ModelSerializer):
+    file_url=serializers.SerializerMethodField()
+    file_type=serializers.SerializerMethodField()
+    class Meta():
+        model=DocumentosModel
+        fields=["id","file_url","file_type","created_at"]
+
+    def get_file_url(self,obj):
+        request=self.context.get("request")
+        if not obj.file:
+            return None
+        return request.build_absolute_uri(obj.file.url)
+
+    def get_file_type(self,obj):
+        name=obj.file.name.lower()
+        if name.endswith(".pdf"):
+            return "pdf"
+        if name.endswith(".doc") or name.endswith(".docx"):
+            return "word"       
+        return "other" 
