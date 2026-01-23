cc01fa749eafcb23004cccea7fb8f8ec2f165e31
joshua <condorenajoshua98@gmail.com>
2026-01-22 21:00:01 -0500

    Se implemneto el api de start y la ui de la pagina

--- fronted/src/Favorite/FavoritePage.tsx
+++ fronted/src/Favorite/FavoritePage.tsx
@@ -1,11 +1,50 @@
-
-
+import { axiosInstance } from "@/lib/axios"
+import { Star } from "lucide-react"
+import { useEffect, useState } from "react"
 export default function FavoritePage() {
+  const refresh=localStorage.getItem('access')
+  const [isloading, setisloading] = useState(false)
+  const [datafavorite, setdatafavorite] = useState([])
+  const GetStartDocuments=async()=>{
+    try{
+      setisloading(true)
+      const {data}=await axiosInstance.get(`/documents/start/get/`,{headers:{
+        Authorization:`Bearer ${refresh}`
+      }})
+      setdatafavorite(data)
+      console.log(data)
+    }catch(err){
+      console.error(err)
+    }finally{
+      setisloading(false)
+    }
+  }
+  useEffect(()=>{
+    GetStartDocuments()
+    
+  },[])
   return (
     <>
     <div className="">
         <div className="">
             <h1 className="text-3xl">Favoritos</h1>
+        </div>
+        <div className="flex flex-row flex-wrap gap-4 mt-4">
+          {datafavorite.map((s)=>{
+          return(
+            <div className="w-100 h-110 overflow-y-scroll overflow-hidden rounded-2xl bg-white p-4" key={s.id}>
+              <div className="mb-4 flex flex-row justify-between items-center">
+                <p className="font-semibold">¿{s.ask}?</p>
+               <div className="">
+                 <Star className={`${s.is_start?"fill-yellow-200":"bg-trarent"}`}></Star>
+               </div>
+              </div>
+              <div className="">
+                <p className="w-80">{s.text}</p>
+              </div>
+            </div>
+          )
+        })}
         </div>
     </div>
     </>
