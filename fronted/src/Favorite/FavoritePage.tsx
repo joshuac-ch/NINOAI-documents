44375585c22550df3e398e0c202e0c49a239e6c3
joshua <condorenajoshua98@gmail.com>
2026-01-25 21:05:21 -0500

    Se modifico la ui de favorite page

--- fronted/src/Favorite/FavoritePage.tsx
+++ fronted/src/Favorite/FavoritePage.tsx
@@ -1,5 +1,7 @@
+import { Button } from "@/components/ui/button"
+import { Menubar, MenubarContent, MenubarGroup, MenubarItem, MenubarMenu, MenubarTrigger } from "@/components/ui/menubar"
 import { axiosInstance } from "@/lib/axios"
-import { Star } from "lucide-react"
+import { Ellipsis, Star } from "lucide-react"
 import { useEffect, useState } from "react"
 export default function FavoritePage() {
   const refresh=localStorage.getItem('access')
@@ -32,15 +34,35 @@ export default function FavoritePage() {
         <div className="flex flex-row flex-wrap gap-4 mt-4">
           {datafavorite.map((s)=>{
           return(
-            <div className="w-100 h-110 overflow-y-scroll overflow-hidden rounded-2xl bg-white p-4" key={s.id}>
+            <div className="md:w-100 rounded-2xl bg-white p-4" key={s.id}>
               <div className="mb-4 flex flex-row justify-between items-center">
                 <p className="font-semibold">¿{s.ask}?</p>
-               <div className="">
-                 <Star className={`${s.is_start?"fill-yellow-200":"bg-trarent"}`}></Star>
+               <div className="p-4">
+                <Button variant={'default'}><Star className={`${s.is_start?"text-yellow-200 fill-yellow-200":"bg-transparent"}`}></Star></Button>
+                 
                </div>
               </div>
-              <div className="">
+              <div className="md:h-110 overflow-hidden overflow-y-scroll">
                 <p className="w-80">{s.text}</p>
+              </div>
+              <div className="flex flex-row justify-between items-center mt-2">
+                <div className="">
+                  <p>{s.created_at?new Date(s.created_at).toLocaleString():""}</p>
+                </div>
+                <div className="">
+                <Menubar>
+                  <MenubarMenu>
+                    <MenubarTrigger><Ellipsis></Ellipsis></MenubarTrigger>
+                    <MenubarContent>
+                      <MenubarGroup>
+                        <MenubarItem>
+                          <a href={s.file_url} target="_blank">Descargar</a>
+                        </MenubarItem>                        
+                      </MenubarGroup>                      
+                    </MenubarContent>
+                  </MenubarMenu>
+                </Menubar>
+                </div>
               </div>
             </div>
           )
