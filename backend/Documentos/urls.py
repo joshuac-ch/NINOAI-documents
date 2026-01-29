4f541c2c19d94ab81685cf40c2c1b22fcaf92863
joshua <condorenajoshua98@gmail.com>
2026-01-28 21:33:48 -0500

    Se modifico el Rag para manejar los documentos y se implemnto modificaciones en las rutas

--- backend/Documentos/urls.py
+++ backend/Documentos/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import helloword,UploadDocumentView
-from .documentserializer import DocumentView
+from .views import helloword,UploadDocumentView,MyDocument,UpdateStartMDocument,StartDocumentGet
+
 
 urlpatterns = [
     path('documentos/',helloword),    
-    path("documentos/upload/",UploadDocumentView.as_view())
+    path("documentos/upload/",UploadDocumentView.as_view()),
+    path("documentos/getall/",MyDocument.as_view()),
+    path("documentos/<int:id>/start/",UpdateStartMDocument.as_view()),
+    path("documents/start/get/",StartDocumentGet.as_view())
 ]
--- backend/Documentos/views.py
+++ backend/Documentos/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import DocumentosModel
 from rest_framework.permissions import IsAuthenticated
 import requests
-
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from .serealizer import DocumentSerealizer,DocuemntStartSerealizer
 # Create your views here.
 SERVICE_IA_URL="http://127.0.0.1:8001"
 
@@ -16,12 +17,13 @@ class UploadDocumentView(APIView):
     permission_classes=[IsAuthenticated]
     def post(self,request):
         file=request.FILES.get('file')
-        
+        ask=request.data.get('pregunta')
         doc=DocumentosModel.objects.create(
             user=request.user,
-            file=file
+            file=file,
+            ask=ask            
         )
-
+        #enviar el documento al modelo
         with open(doc.file.path,"rb") as f:
             response=requests.post(
                 f"{SERVICE_IA_URL}/ingest",
@@ -30,12 +32,16 @@ class UploadDocumentView(APIView):
         data=response.json()   
         #ingestar el pdf
         doc.document_id=data['document_id']
-        doc.save()
+        
+        #preguntar al modelo lo que buscar
         qa_response=requests.post(f"{SERVICE_IA_URL}/ask",params={
             "document_id":doc.document_id,
-            "question":"dame un resumen de esta persona a que se dedica"
+            "question":ask
         })
         answer=qa_response.json().get('answer',"")
+        #guardar respuesta
+        doc.text=answer
+        doc.save()
         return Response({
             "id":doc.id,
             "document_id":doc.document_id,
@@ -53,3 +59,31 @@ class AskDocument(APIView):
             "question":question
         })
         return Response(response.json())    
+
+class MyDocument(APIView):
+    permission_classes=[IsAuthenticated]
+    
+    def get(self,request):
+        docs=DocumentosModel.objects.filter(user=request.user).order_by("created_at")
+        serealizer=DocumentSerealizer(
+            docs,many=True,context={"request":request}
+        ) 
+        return Response(serealizer.data)
+
+
+class UpdateStartMDocument(APIView):
+    permission_classes=[IsAuthenticated]
+    def post(self,request,id):
+        doc=get_object_or_404(DocumentosModel,id=id,user=request.user)
+        doc.is_start=not doc.is_start
+        doc.save()      
+        return Response({"id":doc.id,"is_star":doc.is_start})
+
+class StartDocumentGet(APIView):
+    permission_classes=[IsAuthenticated]
+    def get(self,request):
+        doc=DocumentosModel.objects.filter(
+            user=request.user,
+            is_start=True)
+        serealizer=DocuemntStartSerealizer(doc,many=True,context={"request":request})
+        return Response(serealizer.data,status=200)    
