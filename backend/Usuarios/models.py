b429abcaed69a264dac9fe2540e1daff33eb2990
joshua <condorenajoshua98@gmail.com>
2026-01-28 21:34:44 -0500

    Se implementaron nuevos cambios en el modelo y el serializer usuario se dejo de usar el ShowUser por ser insecesario y se cambiaron las rutas para mejor manejo a traves de auth

--- backend/Usuarios/models.py
+++ backend/Usuarios/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class User(AbstractUser):
     email=models.EmailField(unique=True)
-    profile=models.CharField(max_length=500)
+    profile=models.ImageField(upload_to="users/",null=True,blank=True,default="users/hori.jpg")
 
     USERNAME_FIELD='email'
     REQUIRED_FIELDS=['username']
--- backend/Usuarios/serializer.py
+++ backend/Usuarios/serializer.py
@@ -7,11 +7,26 @@ class RegisterUserSerealizaer(serializers.ModelSerializer):
         model=User
         fields=['email','password','username','first_name','last_name']
     def create(self,validate_data):
-        return User.objects.create(**validate_data)
+        password=validate_data.pop("password")
+        user=User.objects.create(**validate_data)
+        user.set_password(password)
+        user.save()
+        return user
     
 
 class ShowUserSerializer(serializers.ModelSerializer):
+    profile=serializers.SerializerMethodField()
     class Meta:
         model=User
         fields=["email","first_name","last_name","profile","username"]
+    def get_profile(self,obj):
+        if obj.profile and hasattr(obj.profile,"url"):
+            return obj.profile.url
+        return None
+class UpdateUserSerealizer(serializers.ModelSerializer):
+    profile = serializers.ImageField(required=False, allow_null=True)
+    class Meta:
+        model=User
+        fields=["username","first_name","last_name","profile"]
+                
             
